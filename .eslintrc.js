6ef3d2f5053c8aa9296821e8ffa476f970b8521f
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-10-01 08:53:47 +0700

    git init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
+
+// "eslintConfig": {
+//   "root": true,
+//   "env": {
+//     "node": true
+//   },
+//   "extends": [
+//     "plugin:vue/essential",
+//     "eslint:recommended"
+//   ],
+//   "parserOptions": {
+//     "parser": "@babel/eslint-parser"
+//   },
+//   "rules": {}
+// },
+// "browserslist": [
+//   "> 1%",
+//   "last 2 versions",
+//   "not dead"
+// ]
