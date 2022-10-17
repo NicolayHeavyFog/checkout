d987b6c5e2fd51b9af2164d07e513f7bc5d4c970
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-10-17 11:08:40 +0700

    fix colors and price legend

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,15 @@
 export const GREEN = "#A7C42A";
 export const BLUE = "#07237E";
-export const colors = ["#E2A2ED", "#FFCD82", "#FFAF82", "#FF8282"];
+export const colors = [
+  "#E2A2ED",
+  "#FFCD82",
+  "#FFAF82",
+  "#FF8282",
+  "#7fb5b5",
+  "#9d9101",
+  "#382c1e",
+  "#1e2460",
+];
 
 // export default {
 //   green: "#A7C42A",
